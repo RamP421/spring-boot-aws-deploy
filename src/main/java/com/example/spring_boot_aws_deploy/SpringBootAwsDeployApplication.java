8e4fa9f4ed956425caf5a6fc8f7264a456d3178f
Ramya priya <ramyapriyakusuma@gmail.com>
2025-01-16 13:00:53 +0100

    first commit

---
+++ src/main/java/com/example/spring_boot_aws_deploy/SpringBootAwsDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_aws_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_aws_deploy/SpringBootAwsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_aws_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
